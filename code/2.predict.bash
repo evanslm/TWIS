7c7ad8d0c88050df39c32afb3b95ba0aac83942c
evanslm <90649861+evanslm@users.noreply.github.com>
2022-11-14 16:02:19 -0700

    Update 2.predict.bash

--- code/2.predict.bash
+++ code/2.predict.bash
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-tissue="cortex.PEC"
+#### This script creates the predicted expression files from the TWAS weights in the target sample
+
+ncore=6 ### However many cores you have available on your machine
+
+tissue="pfc"
 scores="scores/"$tissue
 study="1kg"
 predicted="predicted/"$tissue"/"$study
@@ -11,9 +15,9 @@ mkdir -p $predicted
 chr=21
 pfile=1kg/eur."$chr".keep
 
-grep -Ff PsychENCODE_TWAS/cortex.PEC/ensgID.txt ensg.ids.hg37.txt | awk '$6==21 {print $1}' | \
-    xargs -i -P $ncore sh -c "plink --pfile '$pfile' --read-freq '$pfile'.afreq --score '$scores'/{}.score 1 2 4 --out '$predicted'/{}.pred"
-    
+grep -Ff PsychENCODE_TWAS/ensgID.txt ensg.ids.hg37.txt | awk '$6==21 {print $1}' | \
+    xargs -i -P $ncore sh -c "plink2 --pfile '$pfile' --read-freq '$pfile'.afreq --score '$scores'/{}.score 1 2 4 --out '$predicted'/{}.pred"
+
 
 ### Get list of all genes with prediction. Using the genes with .sscore prediction files ensures all genes tested will have predictions:
 ### Done only once for each tissue/study combination
