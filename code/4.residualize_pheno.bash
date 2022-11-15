4893de96e43e2e25fa40a1087f88827ceaeb17f3
evanslm <90649861+evanslm@users.noreply.github.com>
2022-11-14 21:55:37 -0700

    Update 4.residualize_pheno.bash

--- code/4.residualize_pheno.bash
+++ code/4.residualize_pheno.bash
@@ -5,7 +5,7 @@
 
 mkdir -p phenos/
 
-phenotype="SIMULATE_binom" ### Either the name of the file (with columns FID,IID,<phenotypename>), or "SIMULATE" for a simulated phenotype
+phenotype="SIMULATE" ### Either the name of the file (with columns FID,IID,<phenotypename>), or "SIMULATE" for a simulated phenotype
 covariate="SIMULATE" ### Either the name of the file (with columns FID,IID,cov1,cov2...), or "SIMULATE" for a simulated simulated covariates
 simpheno="norm" ### can be left out, or "binom" or "norm" for a binomially or normally distributed phenotype, respectively
 
