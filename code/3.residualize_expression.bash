1915786573cfd7322c9105ed4ccadf54e695aa85
Chris Arehart <40525022+char4816@users.noreply.github.com>
2022-11-18 13:54:22 -0700

    Update 3.residualize_expression.bash

--- code/3.residualize_expression.bash
+++ code/3.residualize_expression.bash
@@ -3,6 +3,24 @@
 ### Script to residualize the imputed gene expression data on covariates
 ### Requires R packages: RcppArmadillo and optparse
 
+# % # % # % # % # % # % # % # % # % # % # % # % #
+#  -------------------------------------------  #
+# |               TWIS Pipeline               | #
+# |      University of Colorado Boulder       | #
+# |     Institute for Behavioral Genetics     | #
+# | ========================================= | #
+# |               Luke M. Evans               | #
+# |         luke.m.evans@colorado.edu         | #
+# | ========================================= | #
+# |        Written in November of 2022        | #
+#  -------------------------------------------  #
+# % # % # % # % # % # % # % # % # % # % # % # % #
+
+### start timer for script
+date
+startTime="$(date +%s)"
+
+### setup
 covar="covariates.txt"
 predicted="predicted/"
 residualized="residualized/"
@@ -12,3 +30,22 @@ mkdir -p $residualized
 Rscript expression_residualize.R --pred_scores $predicted --resid_out $residualized --covar SIMULATE > log.3.residualize.outerr 2>&1
 echo "completed residualizing predicted gene expression"
 
+### end timer for script and print the runtime that has elapsed
+echo " *-* *-* *-* *-* *-* END OF SCRIPT *-* *-* *-* *-* *-* "
+date
+endTime="$(date +%s)"
+diff=$(echo "$endTime-$startTime" |bc)
+diff=$(($diff + 0))
+
+if [ $diff -lt "$((60))" ]
+then
+  printf %.3f "$((1000 * $diff/1))e-3"; echo " seconds"
+elif [ $diff -lt "$((60 * 60))" ]
+then
+  printf %.3f "$((1000 * $diff/60))e-3"; echo " minutes"
+elif [ $diff -lt "$((60 * 60 * 24))" ]
+then
+  printf %.3f "$((1000 * $diff/3600))e-3"; echo " hours"
+else
+  printf %.3f "$((1000 * $diff/86400))e-3"; echo " days"
+fi
