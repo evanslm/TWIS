31076c047f6e5c000b3a0a933739ad97764ca5a8
Chris Arehart <40525022+char4816@users.noreply.github.com>
2022-11-18 14:02:31 -0700

    Update 6.twis.bash

--- code/6.twis.bash
+++ code/6.twis.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+### Below is an example slurm header for running this script
+
 #SBATCH -J twas_gxg
 #SBATCH -N 1-1
 #SBATCH --ntasks=8
@@ -8,10 +11,26 @@
 #SBATCH --mem=5000
 
 ### Script to run gxg TWAS analysis using predicted expression values
+### make sure that array number matches the nparts variable, which specifies how many chunks the analysis is split into
 ### Requires the following R libraries: data.table, optparse, Matrix, RcppArmadillo, foreach, iterators, and doMC
 
+# % # % # % # % # % # % # % # % # % # % # % # % #
+#  -------------------------------------------  #
+# |               TWIS Pipeline               | #
+# |      University of Colorado Boulder       | #
+# |     Institute for Behavioral Genetics     | #
+# | ========================================= | #
+# |               Luke M. Evans               | #
+# |         luke.m.evans@colorado.edu         | #
+# | ========================================= | #
+# |        Written in November of 2022        | #
+#  -------------------------------------------  #
+# % # % # % # % # % # % # % # % # % # % # % # % #
 
+### start timer for script
 date
+startTime="$(date +%s)"
+
 hostname
 export OMP_NUM_THREADS=1
 
@@ -41,7 +60,23 @@ Rscript twis.R \
 echo "completed running predicted gene expression gxg for phenotype $phenotype"
 
 
-
-
+### end timer for script and print the runtime that has elapsed
+echo " *-* *-* *-* *-* *-* END OF SCRIPT *-* *-* *-* *-* *-* "
 date
+endTime="$(date +%s)"
+diff=$(echo "$endTime-$startTime" |bc)
+diff=$(($diff + 0))
+
+if [ $diff -lt "$((60))" ]
+then
+  printf %.3f "$((1000 * $diff/1))e-3"; echo " seconds"
+elif [ $diff -lt "$((60 * 60))" ]
+then
+  printf %.3f "$((1000 * $diff/60))e-3"; echo " minutes"
+elif [ $diff -lt "$((60 * 60 * 24))" ]
+then
+  printf %.3f "$((1000 * $diff/3600))e-3"; echo " hours"
+else
+  printf %.3f "$((1000 * $diff/86400))e-3"; echo " days"
+fi
 
